3278d4cb7cfc7c73b598129a13c800d3038cf822
Estebanmual <esteban.munozal@gmail.com>
2022-05-31 15:51:11 -0500

    Added linters

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
-    def index
-    end
-    def show
-        @user = params[:id]
-    end
+  def index; end
+
+  def show
+    @user = params[:id]
+  end
 end
