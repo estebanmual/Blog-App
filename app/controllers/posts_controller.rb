731df9140fcf077db8e93e0d0e31e78431ae2c72
Estebanmual <esteban.munozal@gmail.com>
2022-05-31 15:45:36 -0500

    added routing,  controllers and views

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,9 @@
+class PostsController < ApplicationController
+    def index
+        @user_id = params[:user_id]
+    end
+    def show
+        @user_id = params[:user_id]
+        @post_id = params[:id]
+    end
+end
