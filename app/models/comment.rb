47a04532216d169fe9e6bcc238ad19278d6eb970
Estebanmual <esteban.munozal@gmail.com>
2022-06-02 16:40:10 -0500

    Created the models and joined them through foreign keys

---
+++ app/models/comment.rb
@@ -0,0 +1,6 @@
+class Comment < ApplicationRecord
+    validates :Text, presence: true
+
+    belongs_to :user
+    belongs_to :post
+end
---
+++ app/models/post.rb
@@ -0,0 +1,8 @@
+class Post < ApplicationRecord
+    validates :Title, presence: true
+    validates :Text, presence: true
+    
+    belongs_to :user
+    has_many :likes, dependent: :destroy
+    has_many :comments, dependent: :destroy
+end
