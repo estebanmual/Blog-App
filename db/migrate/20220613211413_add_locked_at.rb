cfcca660acac9e6865e6efa1b8e3ec2196b25197
Estebanmual <esteban.munozal@gmail.com>
2022-06-13 18:24:02 -0500

    Implement code reviewer suggestions

--- db/migrate/20220613211413_add_locked_at.rb
+++
@@ -1,5 +0,0 @@
-class AddLockedAt < ActiveRecord::Migration[7.0]
-  def change
-    add_column :users, :locked_at, :datetime 
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_13_211542) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_13_193104) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -56,17 +56,18 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_13_211542) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.string "confirmation_token"
-    t.datetime "confirmed_at"
-    t.datetime "confirmation_sent_at"
-    t.string "unconfirmed_email"
-    t.datetime "locked_at"
-    t.integer "sign_in_count", default: 0
+    t.integer "sign_in_count", default: 0, null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string "current_sign_in_ip"
     t.string "last_sign_in_ip"
-    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
+    t.integer "failed_attempts", default: 0, null: false
+    t.string "unlock_token"
+    t.datetime "locked_at"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
