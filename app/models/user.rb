ee6f15056cf5c151c999b643181056ce1387ddd2
Kakalanp <jgarridoleccazanetti@gmail.com>
2022-06-14 10:50:25 -0500

    Adding ability

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :confirmable, :lockable, :trackable
-  enum role: [:user, :admin]
-  after_initialize :set_default_role, :if => :new_record?
+  enum role: %i[user admin]
+  after_initialize :set_default_role, if: :new_record?
   def set_default_role
     self.role ||= :user
   end
