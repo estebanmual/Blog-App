e9287993bfb26da1391af10728b19e4af7412c21
Estebanmual <esteban.munozal@gmail.com>
2022-06-14 10:34:41 -0500

    Confirm that roles are working

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,11 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :confirmable, :lockable, :trackable
+  enum role: [:user, :admin]
+  after_initialize :set_default_role, :if => :new_record?
+  def set_default_role
+    self.role ||= :user
+  end
   validates :Name, presence: true
   validates :PostsCounter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
