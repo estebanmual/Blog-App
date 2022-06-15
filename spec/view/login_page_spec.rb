7a36c2b87cbf620d05e5da5367552e5fe0425c43
Estebanmual <esteban.munozal@gmail.com>
2022-06-15 12:22:42 -0500

    Added load tests

--- spec/view/login_page_spec.rb
+++ spec/view/login_page_spec.rb
@@ -14,17 +14,12 @@ RSpec.describe 'login page', type: :feature do
       expect(page).to have_content('Invalid email or password')
     end
 
-    describe 'the signin process', type: :feature do
-      before :each do
-        User.create(email: 'user1@gmail.com', password: 'password', name: 'User1')
-      end
-      it 'signs @user in' do
+      it 'signs in' do
         visit '/users/sign_in'
-        fill_in 'email', with: 'hola@hola.com'
-        fill_in 'password', with: 'password'
+        fill_in 'Email', with: 'hola@hola.com'
+        fill_in 'Password', with: 'mex420'
         click_button 'Log in'
        expect(current_path).to eq(root_path)
        expect(page).to have_text('Signed in successfully.')
       end
-     end    
 end
