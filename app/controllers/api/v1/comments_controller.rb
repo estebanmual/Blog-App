25db1d23f45e58495cf3d58d9a1f4a054499f20a
Estebanmual <esteban.munozal@gmail.com>
2022-06-16 16:57:45 -0500

    Added comment controller for api v1

---
+++ app/controllers/api/v1/comments_controller.rb
@@ -0,0 +1,23 @@
+class Api::V1::CommentsController < ApplicationController
+    def index
+        user = User.find(params[:user_id])
+        post = user.posts.find(params[:post_id])
+
+        render json: post.comments
+    end
+
+    def create
+        comment = current_user.comments.new(comment_params)
+        
+        if comment.save
+            render json: comment
+        end
+    end
+
+    private
+
+    def comments_params
+        defaults = { post_id: params[:post_id] }
+        params.require(:comment).permit(:Text, :post_id).reverse_merge(defaults)
+    end
+end
