8c338a432bd75daed64ab35a5e2604f050badc31
Estebanmual <esteban.munozal@gmail.com>
2022-06-16 16:56:14 -0500

    Added APi namespaces in routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: {registrations: :registrations, sessions: :sessions}
+  namespace :api do
+    namespace :v1 do
+      resources :users, path: '/users' do
+        resources :posts, path: '/posts' do
+          resources :comments, only: [:new, :create, :destroy]
+          resources :likes, only: [:new, :create]
+        end
+      end
+    end
+  end
   root 'users#index'
   resources :users, path: '/users' do
     resources :posts, path: '/posts' do
